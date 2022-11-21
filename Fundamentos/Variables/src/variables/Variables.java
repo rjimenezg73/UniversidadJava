aeaadded96596e96ccdbcb746027b994d58e72e3
Roberto Jimenez <robertojimenezg@gmail.com>
2022-11-20 18:32:54 -0600

    Uso de variables

---
+++ Fundamentos/Variables/src/variables/Variables.java
@@ -0,0 +1,19 @@
+
+public class Variables {
+
+  public static void main(String[] args) {
+    // TODO code application logic here
+    int miVariableEntera = 10;
+    System.out.println(miVariableEntera); 
+    
+    miVariableEntera = 5;
+    System.out.println(miVariableEntera);
+    
+    miVariableEntera = 20;
+    System.out.println(miVariableEntera);
+    
+    String miVariableCadena = "Saludos";
+    System.out.println(miVariableCadena);
+  }
+  
+}
